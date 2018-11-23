95fb328f98f7c2f7f7139acdf295fde5b21486b2
Andrew Shillito <andrew.shillito88@gmail.com>
2018-11-23 20:14:14 +0000

    change action-map description

--- src/utils/action-map.js
+++ src/utils/action-map.js
@@ -5,7 +5,7 @@ const actionPath = [
         "if async func - once resolved it goes to next step",
         "actionCreator func is called - returns object",
         "object is dispatched to store which calls reducer",
-        "appropriate reducer finds action.type and executes code therein to update store",
+        "appropriate reducer finds action.type and executes code therein",
         "reducer returns new store",
         "ui is updated"
     ];
