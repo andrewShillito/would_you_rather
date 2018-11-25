47ed17655cc5d51b23bbadbd224609108c596ce5
Andrew Shillito <andrew.shillito88@gmail.com>
2018-11-25 16:03:09 +0000

    feat: start implmenting VoteReport progress-bar sizing

--- src/components/VoteReport.js
+++ src/components/VoteReport.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 
 class VoteReport extends Component {
     render() {
+        
+        const percent = (this.props.numVotes/this.props.totalVotes)*100;
+        
         return (
             <div className={this.props.selected ? "answer-selected" : ""}>
                 <h3>{this.props.text}</h3>
                 <div className="progress-bar">
-                    <div className="progress-bar-inner"></div>
+                    <div className="progress-bar-inner" width={`${percent}%`}></div>
                 </div>
                 <p>{`${this.props.numVotes} out of ${this.props.totalVotes} votes`}</p>
             </div>
