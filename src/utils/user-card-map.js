fb13097af402d786107f668580cb63ee319968ed
Andrew Shillito <andrew.shillito88@gmail.com>
2018-11-25 18:37:34 +0000

    docs: doc possible user-card component to replace answer/q/qpreview

---
+++ src/utils/user-card-map.js
@@ -0,0 +1,9 @@
+const userCard = {
+    div: {
+        title: String, // title passed by component above in heirarchy
+        avatar: String, // url passed by component above in heirarchy - then passed to <Avatar />
+    }
+}
+
+// userCard could be used on multiple routes
+// children of userCard determined by the route it is on?
