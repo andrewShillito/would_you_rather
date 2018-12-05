79ea1466956ef751c06054a7d4091bfca5d9dd42
Andrew Shillito <andrew.shillito88@gmail.com>
2018-12-05 20:47:54 +0000

    chore: remove Title component import from New component

--- src/components/New.js
+++ src/components/New.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleCreateQuestion } from "../actions/questions";
-import Title from "./Title";
 import { withRouter } from "react-router-dom"
 
 class New extends Component {
