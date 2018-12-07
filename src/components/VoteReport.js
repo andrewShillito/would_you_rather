cc36e535e8595397ca2c5a71f7f264109d6d5667
Andrew Shillito <andrew.shillito88@gmail.com>
2018-12-07 17:40:17 +0000

    bug: prevent percent display in progress bar when percent === 0

--- src/components/VoteReport.js
+++ src/components/VoteReport.js
@@ -4,14 +4,24 @@ class VoteReport extends Component {
     render() {
 
         const percent = Math.round((this.props.numVotes/this.props.totalVotes)*100);
+        const width = percent ? `${percent}%` : '0%';
+        
+        const innerStyle = {
+            width,
+            height: "100%",
+            backgroundColor: "#439ef9",
+        }
 
         return (
             <div className={this.props.selected ? "vote-report selected" : "vote-report"}>
                 <h3>{this.props.text}
                 </h3>
                 <div className="progress-bar">
-                    <div className="progress-bar-inner" style={{width: `${percent}%`, height: "100%", backgroundColor: "#439ef9"}}>
-                        <span className="progress-bar-text">{`${percent}%`}</span>
+                    <div className="progress-bar-inner" style={innerStyle}>
+                        {percent 
+                            ? <span className="progress-bar-text">{`${percent}%`}</span>
+                            : null
+                        }
                     </div>
                 </div>
                 <p>{`${this.props.numVotes} out of ${this.props.totalVotes} votes`}</p>
