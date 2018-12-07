71d33fb31d828cfb3c3f2755d4d2d6bc3fc7456c
Andrew Shillito <andrew.shillito88@gmail.com>
2018-12-07 17:32:00 +0000

    feat: answer component styling

--- src/components/VoteReport.js
+++ src/components/VoteReport.js
@@ -3,13 +3,16 @@ import React, { Component } from "react";
 class VoteReport extends Component {
     render() {
 
-        const percent = (this.props.numVotes/this.props.totalVotes)*100;
+        const percent = Math.round((this.props.numVotes/this.props.totalVotes)*100);
 
         return (
             <div className={this.props.selected ? "vote-report selected" : "vote-report"}>
-                <h3>{this.props.text}</h3>
+                <h3>{this.props.text}
+                </h3>
                 <div className="progress-bar">
-                    <div className="progress-bar-inner" width={`${percent}%`}></div>
+                    <div className="progress-bar-inner" style={{width: `${percent}%`, height: "100%", backgroundColor: "#439ef9"}}>
+                        <span className="progress-bar-text">{`${percent}%`}</span>
+                    </div>
                 </div>
                 <p>{`${this.props.numVotes} out of ${this.props.totalVotes} votes`}</p>
             </div>
