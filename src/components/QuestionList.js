5cfac4d1c92cdc717dfa0198af68a97e99c9370e
Andrew Shillito <andrew.shillito88@gmail.com>
2018-12-27 14:54:05 +0000

    feat: sort question list by timestamp - newest first

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -61,9 +61,9 @@ class QuestionList extends Component {
 function mapStateToProps({ questions, users, authedUser }) {
 
     const user = users[authedUser];
-    const answered = Object.keys(user.answers).map((qid) => questions[qid]);
+    const answered = Object.keys(user.answers).map((qid) => questions[qid]).sort((a, b) => b.timestamp - a.timestamp);
     const unanswered = Object.keys(questions).filter((question) => !(question in user.answers))
-            .map((qid) => questions[qid]);
+            .map((qid) => questions[qid]).sort((a, b) => b.timestamp - a.timestamp);
 
     return {
         answered,
